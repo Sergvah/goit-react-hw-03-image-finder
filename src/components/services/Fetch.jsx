75c801f55cbe02d355c2bf1d12f00f4df9c46af1
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-12-18 13:34:43 +0200

    fetch-

--- src/components/services/Fetch.jsx
+++ src/components/services/Fetch.jsx
@@ -5,14 +5,26 @@
 // //     return response;
 // //   });
 
+// const response = (inputName, page) => {
+//   console.log(inputName, page);
+//   fetch(
+//     `https://pixabay.com/api/?q=${inputName}&page=${page}&key=30566822-5dd8c7f8088312f63e039c329&image_type=photo&orientation=horizontal&per_page=12`
+//   ).then(res => {
+//     console.log(res);
+//     return res;
+//   });
+// };
+// export default response;
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '30566822-5dd8c7f8088312f63e039c329';
+
 const response = (inputName, page) => {
-  console.log(inputName, page);
-  fetch(
-    `https://pixabay.com/api/?q=${inputName}&page=${page}&key=30566822-5dd8c7f8088312f63e039c329&image_type=photo&orientation=horizontal&per_page=12`
-  ).then(res => {
-    console.log(res);
-    return res;
-  });
+  return fetch(
+    `${BASE_URL}/?key=${API_KEY}&q=${inputName}
+      &image_type=photo&orientation=horizontal&page=
+      ${page}&per_page=12`
+  ).then(response => response.json());
 };
 export default response;
 
